fdd94a60137c49d5d8a3c229db2fd7af33f9ce52
WhiteMemory99 <lisen1510@gmail.com>
2022-09-21 06:17:31 +0700

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.6.20"
-    id("org.jetbrains.intellij") version "1.7.0"
+    id("org.jetbrains.intellij") version "1.9.0"
     id("org.jetbrains.changelog") version "1.3.1"
 }
 
