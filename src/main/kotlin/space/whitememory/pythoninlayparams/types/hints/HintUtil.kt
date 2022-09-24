93f59d6b19f394341d290c374da788b27823e60c
Divaltor <kuklavudu78@gmail.com>
2022-09-24 19:39:39 +0300

    Minor refactoring for binary expressions

--- src/main/kotlin/space/whitememory/pythoninlayparams/types/hints/HintUtil.kt
+++ src/main/kotlin/space/whitememory/pythoninlayparams/types/hints/HintUtil.kt
@@ -1,7 +1,11 @@
 package space.whitememory.pythoninlayparams.types.hints
 
+import com.jetbrains.python.psi.PyBinaryExpression
+import com.jetbrains.python.psi.PyConditionalExpression
+import com.jetbrains.python.psi.PyExpression
 import com.jetbrains.python.psi.PyNamedParameter
 import com.jetbrains.python.psi.PyParameter
+import kotlin.math.exp
 
 sealed class HintUtil {
     companion object {
@@ -14,6 +18,29 @@ sealed class HintUtil {
                     "${parameter.name}: ${parameter.annotationValue}"
                 } ?: parameter.name
             }
+        }
+    }
+}
+
+
+data class ExpressionOperands(val leftOperand: PyExpression, val rightOperand: PyExpression?) {
+    companion object {
+        fun fromPyExpression(expression: PyExpression): ExpressionOperands? {
+            if (expression is PyBinaryExpression) {
+                return ExpressionOperands(
+                    leftOperand = expression.leftExpression,
+                    rightOperand = expression.rightExpression
+                )
+            }
+
+            if (expression is PyConditionalExpression) {
+                return ExpressionOperands(
+                    leftOperand = expression.truePart,
+                    rightOperand = expression.falsePart
+                )
+            }
+
+            return null
         }
     }
 }
